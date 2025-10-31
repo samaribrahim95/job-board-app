db3877ac7a70c49dd0e670b1a549160245cb7398
Samar Ibrahim <samaribrahim@Samars-iMac.local>
2025-10-31 15:53:23 +0300

    add filter by name & category

---
+++ src/components/Filter.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useJobsStore } from "../store/jobs"
+import { Job } from "../types/jobs";
+
+type Props = {
+  onFilter: (filteredJobs: Job[]) => void
+}
+
+const Filter = (props: Props) => {
+  const {jobs} = useJobsStore();
+  const [cats, setCats] = useState<string[]>([]);
+  const [jobTitle, setJobTitle] = useState<string>('')
+  const [selectedCat, setSelectedCat] = useState<string>('')
+
+  useEffect(() => {
+    setCats(jobs?.map(job => job.category) ?? [])
+  },[jobs])
+
+  const filterByName = (e:  React.ChangeEvent<HTMLInputElement>) => {
+    const {value} = e.currentTarget
+    setJobTitle(value);
+    filterJobs(value, selectedCat)
+  }
+
+  const filterByCat = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const {value} = e.currentTarget
+    setSelectedCat(value);
+    filterJobs(jobTitle, value)
+  }
+
+  const filterJobs = (title: string, cat:string) => {
+    props.onFilter(jobs?.filter(job => job.title.toLowerCase().includes(title.toLowerCase()) && job.category.includes(cat)) ?? [])
+  }
+
+  return <div className="grid grid-cols-3 gap-3 my-5">
+    <div className="col-span-2">
+      <input 
+        type="text" 
+        value={jobTitle} 
+        placeholder="Search By Job Title..." 
+        className="w-full p-4 border border-zinc-200 shadow-sm rounded-lg"
+        onChange={(e) =>  filterByName(e)}
+      />
+    </div>
+    <div className="col-span-1">
+      <select className="w-full p-4 border border-zinc-200 shadow-sm rounded-lg" onChange={(e) => filterByCat(e)}>
+        <option value=''>All</option>
+        {
+          cats.map((cat, index) => <option key={index} value={cat}>{cat}</option>)
+        }
+      </select>
+    </div>
+  </div>
+}
+
+export default Filter
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchJobs } from "../api/jobsApi";
 import { Job } from "../types/jobs";
 import { useJobsStore } from "../store/jobs";
+import { Link } from "react-router";
+import Filter from "../components/Filter";
 
 const Home = () => {
-  const {jobs, setJobs} = useJobsStore();
+  const {setJobs} = useJobsStore();
+  const [filteredJobs, setfilteredJobs] = useState<Job[]>([])
+
    useEffect(() => {
     fetchJobs({
-      successCallback : (data: Job[]) => setJobs(data)
+      successCallback : (data: Job[]) => {
+        setJobs(data)
+        setfilteredJobs(data)
+      }
     });
   }, [])
+
   return (
     <div className="container mx-auto">
+      <Filter onFilter={(jobs) => setfilteredJobs(jobs)} />
       {
-        jobs?.map((job, index) => 
+        filteredJobs?.map((job, index) => 
           <div key={index} className="border border-zinc-100 shadow-lg rounded-lg my-10 p-5 bg-zinc-50">
-            <h3 className="text-xl font-bold">{job.title}</h3>
+            <Link to={`/job/${job.id}`}><h3 className="text-xl font-bold">{job.title}</h3></Link>
             <p className="text-zinc-500 font-medium mb-3">{job.company}</p>
             <p className="mb-1">{job.description}</p>
-            <p className="text-sm text-zinc-500">{job.location}</p>
+            <p className="text-sm text-zinc-500 mb-3">{job.location}</p>
+            <Link to={`/job/${job.id}`} className="font-medium">Read More</Link>
           </div>
         )
       }
